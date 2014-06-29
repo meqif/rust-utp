95ffd90d308446fde04d96a10cc6ee44d3fa09aa
Ricardo Martins <ricardo@scarybox.net>
2014-06-29 12:05:48 +0100

    Echo server.

--- torresmo.rs
+++ torresmo.rs
@@ -1,25 +1,44 @@
-use std::io::{TcpListener, TcpStream, BufferedReader};
+use std::io::{TcpListener, TcpStream};
 use std::io::{Acceptor, Listener};
 
-fn main() {
-    println!("hello?");
+fn connection_handler(stream: TcpStream) {
+    let mut sock = stream;
+    println!("New client {}", sock.peer_name().unwrap());
 
-    let socket = TcpListener::bind("127.0.0.1", 8080);
+    // Read request
+    let mut buf = [0,..512];
 
-    let mut acceptor = socket.listen();
-
-    for stream in acceptor.incoming() {
-        match stream {
-            Err(e) => {}
-            Ok(stream) => spawn(proc() {
-                println!("New client!");
-                let mut socket = BufferedReader::new(stream);
-                let content = socket.read_to_str();
-                println!("{}", content.unwrap());
-                println!("Gone!")
-            })
+    match sock.read(buf) {
+        Err(e) => { println!("Error detected! {}", e) }
+        Ok(count) => {
+            println!("Received {} bytes:", count);
+            println!("{}", std::str::from_utf8(buf));
         }
     }
 
-    drop(acceptor)
+    // Answer request
+    match sock.write(buf) {
+        _ => {}
+    }
+
+    println!("Gone!");
+    drop(sock);
+}
+
+fn main() {
+    // Create socket
+    let socket = TcpListener::bind("127.0.0.1", 8080);
+
+    // Listen for new connections
+    let mut acceptor = socket.listen();
+
+    // Spawn a new process for handling each incoming connection
+    for stream in acceptor.incoming() {
+        match stream {
+            Err(e) => { println!("{}", e) }
+            Ok(stream) => spawn(proc() { connection_handler(stream) })
+        }
+    }
+
+    drop(acceptor);
 }
