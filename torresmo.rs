a2d894f1943f0c23d227526bf5b8a310eba3f124
Ricardo Martins <ricardo@scarybox.net>
2014-06-30 13:20:37 +0100

    Implement and use UtpPacket.

--- torresmo.rs
+++ torresmo.rs
@@ -22,6 +22,29 @@ impl UtpPacketHeader {
             return buf.as_slice();
         }
     }
+
+    fn len(&self) -> uint {
+        return 24;
+    }
+}
+
+#[allow(dead_code)]
+struct UtpPacket {
+    header: UtpPacketHeader,
+    payload: Vec<u8>,
+}
+
+impl UtpPacket {
+    fn bytes(&self) -> Vec<u8> {
+        let mut buf: Vec<u8> = Vec::with_capacity(self.len());
+        buf.push_all(self.header.bytes());
+        buf.push_all(self.payload.as_slice());
+        return buf;
+    }
+
+    fn len(&self) -> uint {
+        self.header.len() + self.payload.len()
+    }
 }
 
 fn connection_handler(stream: TcpStream) {
@@ -52,22 +75,25 @@ fn connection_handler(stream: TcpStream) {
 }
 
 fn main() {
-    let header = UtpPacketHeader {
-        ver_type: 0 | 0 << 4,
-        extension: 0,
-        connection_id: 0,
-        timestamp_microseconds: 0,
-        timestamp_difference_microseconds: 0,
-        wnd_size: 0,
-        seq_nr: 0,
-        ack_nr: 0,
+    let packet = UtpPacket {
+        header: UtpPacketHeader {
+            ver_type: 0 | 0 << 4,
+            extension: 0,
+            connection_id: 0,
+            timestamp_microseconds: 0,
+            timestamp_difference_microseconds: 0,
+            wnd_size: 0,
+            seq_nr: 0,
+            ack_nr: 0,
+        },
+        payload: String::from_str("Hello\n").into_bytes(),
     };
 
     let mut buf = [0, ..512];
     let mut sock = UdpSocket::bind(SocketAddr { ip: Ipv4Addr(127,0,0,1), port: 8080 }).unwrap();
     match sock.recvfrom(buf) {
         Ok((_, src)) => {
-            let _ = sock.sendto(header.bytes(), src);
+            let _ = sock.sendto(packet.bytes().as_slice(), src);
         }
         Err(_) => {}
     }
