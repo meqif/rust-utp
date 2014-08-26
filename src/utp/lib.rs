e6a29de825ff1c665a0a57f0cdfa83c160ce66ba
Ricardo Martins <ricardo@scarybox.net>
2014-08-26 14:46:53 +0100

    Fix test.

--- src/utp/lib.rs
+++ src/utp/lib.rs
@@ -1697,25 +1697,25 @@ mod test {
             let mut s = client.socket;
             let mut window: Vec<UtpPacket> = Vec::new();
 
-            let mut i = 0;
-            for data in Vec::from_fn(12, |idx| idx as u8 + 1).as_slice().chunks(3) {
+            for (i, data) in Vec::from_fn(12, |idx| idx as u8 + 1).as_slice().chunks(3).enumerate() {
                 let mut packet = UtpPacket::new().wnd_size(BUF_SIZE as u32);
                 packet.set_type(ST_DATA);
                 packet.header.connection_id = client.sender_connection_id.to_be();
-                packet.header.seq_nr = (client.seq_nr + i).to_be();
+                packet.header.seq_nr = client.seq_nr.to_be();
                 packet.header.ack_nr = client.ack_nr.to_be();
                 packet.payload = Vec::from_slice(data);
                 window.push(packet.clone());
                 client.send_window.push(packet.clone());
-                i += 1;
+                client.seq_nr += 1;
             }
 
             let mut packet = UtpPacket::new().wnd_size(BUF_SIZE as u32);
             packet.set_type(ST_FIN);
             packet.header.connection_id = client.sender_connection_id.to_be();
-            packet.header.seq_nr = (client.seq_nr + i).to_be();
+            packet.header.seq_nr = client.seq_nr.to_be();
             packet.header.ack_nr = client.ack_nr.to_be();
             window.push(packet);
+            client.seq_nr += 1;
 
             iotry!(s.send_to(window[3].bytes().as_slice(), serverAddr));
             iotry!(s.send_to(window[2].bytes().as_slice(), serverAddr));
