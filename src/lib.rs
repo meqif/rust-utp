78422783b9f798101ed31cecd1b5149ef063bfdb
Ricardo Martins <ricardo@scarybox.net>
2015-04-30 22:34:08 +0100

    Remove duplicate to-do list.

--- src/lib.rs
+++ src/lib.rs
@@ -45,18 +45,6 @@
 //! }
 //! ```
 
-//   __________  ____  ____
-//  /_  __/ __ \/ __ \/ __ \
-//   / / / / / / / / / / / /
-//  / / / /_/ / /_/ / /_/ /
-// /_/  \____/_____/\____/
-//
-// - Lossy UDP socket for testing purposes: send and receive ops are wrappers
-// that stochastically drop or reorder packets.
-// - Sending FIN on drop
-// - Handle packet loss
-// - Path MTU discovery (RFC4821)
-
 #![deny(missing_docs)]
 
 extern crate rand;
