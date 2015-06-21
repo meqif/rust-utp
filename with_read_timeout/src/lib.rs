c7468d77047b33ea77e3f9d4e4325763ae8e4046
Ricardo Martins <ricardo@scarybox.net>
2015-06-21 12:45:37 +0100

    Add documentation.

--- with_read_timeout/src/lib.rs
+++ with_read_timeout/src/lib.rs
@@ -6,7 +6,11 @@ extern crate libc;
 use std::io::Result;
 use std::net::{UdpSocket, SocketAddr};
 
+/// A trait to make time-limited reads from socket-like objects.
 pub trait WithReadTimeout {
+    /// Receives data from the object, blocking for at most the specified number of milliseconds.
+    /// On success, returns the number of bytes read and the address from whence the data came.  If
+    /// the timeout expires, it returns `ErrorKind::WouldBlock`.
     fn recv_timeout(&mut self, &mut [u8], i64) -> Result<(usize, SocketAddr)>;
 }
 
@@ -25,6 +29,7 @@ impl WithReadTimeout for UdpSocket {
     }
 
     #[cfg(windows)]
+    /// TODO: Set timeout on socket
     fn recv_timeout(&mut self, buf: &mut [u8], _timeout: i64) -> Result<(usize, SocketAddr)> {
         self.recv_from(buf)
     }
