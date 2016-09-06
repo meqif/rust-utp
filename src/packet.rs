2c742cb1850fe9856e66a57905fcec9521c07e28
Ricardo Martins <ricardo@scarybox.net>
2016-09-06 11:31:06 +0100

    Remove redundant extension iterator checks

--- src/packet.rs
+++ src/packet.rs
@@ -426,35 +426,28 @@ impl<'a> ExtensionIterator<'a> {
 
 impl<'a> Iterator for ExtensionIterator<'a> {
     type Item = Extension<'a>;
+
     fn next(&mut self) -> Option<Self::Item> {
         if self.next_extension == ExtensionType::None {
-            return None;
-        }
-
-        if self.index < self.raw_bytes.len() && self.next_extension != ExtensionType::None {
+            None
+        } else if self.index < self.raw_bytes.len() {
             let len = self.raw_bytes[self.index + 1] as usize;
             let extension_start = self.index + 2;
             let extension_end = extension_start + len;
 
             // Assume extension is valid because the bytes come from a (valid) Packet
-            // ...
+            let extension = Extension {
+                ty: self.next_extension,
+                data: &self.raw_bytes[extension_start..extension_end],
+            };
 
-            if self.next_extension != ExtensionType::None {
-                let extension = Extension {
-                    ty: self.next_extension,
-                    data: &self.raw_bytes[extension_start..extension_end],
-                };
+            self.next_extension = self.raw_bytes[self.index].into();
+            self.index += len + 2;
 
-                self.next_extension = self.raw_bytes[self.index].into();
-                self.index += len + 2;
-
-                return Some(extension);
-            }
-
-            return None;
+            Some(extension)
+        } else {
+            None
         }
-
-        None
     }
 }
 
